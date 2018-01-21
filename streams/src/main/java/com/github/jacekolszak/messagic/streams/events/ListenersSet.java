a9eb89c83684b8eb6b5e20cb5c5dbd9e2305bc57
Jacek Olszak <jacekolszak@gmail.com>
2018-01-21 01:20:14 +0100

    Hide ListenersSet methods

--- streams/src/main/java/com/github/jacekolszak/messagic/streams/events/ListenersSet.java
+++ streams/src/main/java/com/github/jacekolszak/messagic/streams/events/ListenersSet.java
@@ -8,25 +8,25 @@ import java.util.function.Consumer;
 
 import com.github.jacekolszak.messagic.Event;
 
-class ListenersSet {
+final class ListenersSet {
 
     private Map<Class<? extends Event>, Set<Consumer<? extends Event>>> map = new HashMap<>();
 
-    public void add(Class<? extends Event> eventClass, Consumer<? extends Event> listener) {
+    void add(Class<? extends Event> eventClass, Consumer<? extends Event> listener) {
         if (!map.containsKey(eventClass)) {
             map.put(eventClass, new LinkedHashSet<>());
         }
         map.get(eventClass).add(listener);
     }
 
-    public void remove(Class<? extends Event> eventClass, Consumer<? extends Event> listener) {
+    void remove(Class<? extends Event> eventClass, Consumer<? extends Event> listener) {
         Set<Consumer<? extends Event>> consumers = map.get(eventClass);
         if (consumers != null) {
             consumers.remove(listener);
         }
     }
 
-    public <T extends Event> Set<Consumer<T>> listenersOfType(Class<T> eventClass) {
+    <T extends Event> Set<Consumer<T>> listenersOfType(Class<T> eventClass) {
         return (Set) map.getOrDefault(eventClass, Set.of());
     }
 
